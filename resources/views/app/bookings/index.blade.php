5acb391e64b6d34877e47f9b965cb6b821480d9a
sakil <sksakilhosan@gmail.com>
2023-07-30 08:19:00 +0530

    minor fixing

--- resources/views/app/bookings/index.blade.php
+++ resources/views/app/bookings/index.blade.php
@@ -15,10 +15,10 @@
         <div class="card-body">
             <div class="row form-group">
                 <div class="col-md-6">
-                    <h4 class="card-title text-uppercase">Time Slots</h4>
+                    <h4 class="card-title text-uppercase">Booking List</h4>
                 </div>
                 <div class="col-md-6 text-right">
-                    <a href="{{ route('timeslots.create') }}" class="btn btn-sm btn-primary">Add</a>
+{{--                    <a href="{{ route('property.create') }}" class="btn btn-sm btn-primary">Add</a>--}}
                 </div>
             </div>
             <div class="table-responsive">
@@ -26,26 +26,35 @@
                     <thead class="thead-dark">
                     <tr>
                         <th width="5%">#</th>
-                        <th>From Time</th>
-                        <th>To Time</th>
-                        <th>Status</th>
+                        <th>User</th>
+                        <th>Property</th>
+                        <th>Check In</th>
+                        <th>Check Out</th>
+                        <th>PAX</th>
+                        <th>Amount</th>
                         <th width="10%">Actions</th>
                     </tr>
                     </thead>
                     <tbody>
-                    @if($time_slots->count() > 0)
-                        @foreach($time_slots as $key => $val)
+                    @if($bookings->count() > 0)
+                        @foreach($bookings as $key => $val)
                             <tr>
                                 <th>{{ 1+ $key }}</th>
                                 <td>
-                                    {{ $val->from_time }}
+                                    {{ $val->user->name }}
                                 </td>
-                                <td>{{ $val->to_time }}</td>
-                                <td>{{ $val->status }}</td>
+                                <td>{{ $val->property->name }}</td>
+                                <td>{{ $val->check_in->format('d-m-Y') }}</td>
+                                <td>{{ $val->check_out->format('d-m-Y') }}</td>
+                                <td>{{ $val->pax }}</td>
+                                <td>{{ $val->total_amount }}</td>
 
                                 <td>
                                     <div class="btn-group">
-                                        <a href="{{ route('timeslots.show',$val->id) }}" class="btn btn-sm btn-outline-primary">View</a>
+                                        @can('booking show')
+                                            <a href="{{ route('bookings.show',$val->id) }}" class="btn btn-sm btn-outline-primary">View</a>
+                                        @endcan
+
                                     </div>
 
                                 </td>
